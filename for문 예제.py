d7f5d480f312bf05998122f09a2a15af5f60f4c8
KIM DOOWON <83748240+KIM-DOOWON@users.noreply.github.com>
2021-11-28 23:46:02 +0900

    Update and rename KIM_99 to for문 예제.py

---
+++ for문 예제.py
@@ -0,0 +1,11 @@
+public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+n = 7
+
+for i in range(n):
+    st= ''
+    for j in range(n-(i+1)):
+        st = st + ' '
+
+    print(st + '#')
